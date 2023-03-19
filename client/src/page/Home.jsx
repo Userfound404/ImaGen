44b54c8268bba7ced08096a6e3f602d31fc22ae8
Adithya Penagonda <97509220+Userfound404@users.noreply.github.com>
2023-03-19 11:19:17 +0530

    Update Home.jsx

--- client/src/page/Home.jsx
+++ client/src/page/Home.jsx
@@ -26,7 +26,7 @@ const Home = () => {
     setLoading(true);
 
     try {
-      const response = await fetch('https://imagen-lskv.onrender.com/api/v1/post', {
+      const response = await fetch('http://imagen-lskv.onrender.com/api/v1/post', {
         method: 'GET',
         headers: {
           'Content-Type': 'application/json',
